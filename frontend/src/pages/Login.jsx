5200f910991c53aaf7d3d4c4ff951468355e4d85
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-11-19 15:22:20 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = ({ setToken, setUserId }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('http://backend:5000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
